48aa532271d908a687d0a266327ff8fb20f37a20
13wjdgk <13wjdgk@naver.com>
2024-11-15 19:45:55 +0900

    feat : 기본 세팅

---
+++ src/test/java/com/daengdaeng_eodiga/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daengdaeng_eodiga.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
