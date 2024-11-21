7680b1702bc2fa38a849b55a55341d30641c90eb
13wjdgk <13wjdgk@naver.com>
2024-11-21 12:16:24 +0900

    test : test h2 DB 연결

--- src/test/java/com/daengdaeng_eodiga/project/ProjectApplicationTests.java
+++ src/test/java/com/daengdaeng_eodiga/project/ProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.daengdaeng_eodiga.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectApplicationTests {
 
 	@Test
