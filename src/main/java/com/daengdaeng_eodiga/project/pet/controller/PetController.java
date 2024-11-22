1fe581c9a9c871401564d33b595caa3e4f52d37c
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-11-22 13:02:35 +0900

    feat: 반려동물 등록, 수정 API 구현

---
+++ src/main/java/com/daengdaeng_eodiga/project/pet/controller/PetController.java
@@ -0,0 +1,31 @@
+package com.daengdaeng_eodiga.project.pet.controller;
+
+import com.daengdaeng_eodiga.project.Global.dto.ApiResponse;
+import com.daengdaeng_eodiga.project.pet.dto.PetRegisterDto;
+import com.daengdaeng_eodiga.project.pet.dto.PetUpdateDto;
+import com.daengdaeng_eodiga.project.pet.service.PetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/pets")
+@RequiredArgsConstructor
+public class PetController {
+    private final PetService petService;
+
+    // TODO : header 로부터 유저정보 가져오는 걸로 변경해야 함
+    private final int hardcodedUserId = 3;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<?>> registerPet(@RequestBody PetRegisterDto requestDto) {
+        petService.registerPet(hardcodedUserId, requestDto);
+        return ResponseEntity.ok(ApiResponse.success("pet inserted succesfully"));
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse<?>> updatePet(@RequestBody PetUpdateDto updateDto) {
+        petService.updatePet(updateDto);
+        return ResponseEntity.ok(ApiResponse.success("pet updated succesfully"));
+    }
+}
--- src/main/java/com/daengdaeng_eodiga/project/pet/service/PetService.java
+++ src/main/java/com/daengdaeng_eodiga/project/pet/service/PetService.java
@@ -1,8 +1,17 @@
 package com.daengdaeng_eodiga.project.pet.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import com.daengdaeng_eodiga.project.Global.exception.DateNotFoundException;
+import com.daengdaeng_eodiga.project.Global.exception.UserNotFoundException;
+import com.daengdaeng_eodiga.project.common.repository.CommonCodeRepository;
+import com.daengdaeng_eodiga.project.pet.dto.PetRegisterDto;
+import com.daengdaeng_eodiga.project.pet.dto.PetUpdateDto;
+import com.daengdaeng_eodiga.project.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import com.daengdaeng_eodiga.project.Global.exception.PetNotFoundException;
@@ -16,6 +25,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PetService {
 	private final PetRepository petRepository;
+	private final UserRepository userRepository;
+	private final CommonCodeRepository commonCodeRepository;
 
 	public List<Pet> fetchUserPets(User user) {
 		return petRepository.findAllByUser(user);
@@ -27,5 +38,52 @@ public class PetService {
 			throw new PetNotFoundException();
 		});
 		return userPets;
+	}
+
+	public void registerPet(int userId, PetRegisterDto requestDto) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(UserNotFoundException::new);
+
+		Pet pet = Pet.builder()
+				.name(requestDto.getName())
+				.image(requestDto.getImage())
+				.species(requestDto.getSpecies())
+				.gender(requestDto.getGender())
+				.size(requestDto.getSize())
+				.birthday(parseDate(requestDto.getBirthday()))
+				.neutering(requestDto.getNeutering())
+				.user(user)
+				.build();
+
+		petRepository.save(pet);
+	}
+
+	public void updatePet(PetUpdateDto updateDto) {
+		Pet pet = petRepository.findById(updateDto.getPetId())
+				.orElseThrow(PetNotFoundException::new);
+
+		pet.setName(updateDto.getName());
+		pet.setImage(updateDto.getImage());
+		pet.setSpecies(updateDto.getSpecies());
+		pet.setGender(updateDto.getGender());
+		pet.setSize(updateDto.getSize());
+		pet.setBirthday(parseDate(updateDto.getBirthday()));
+		pet.setNeutering(updateDto.getNeutering());
+
+		petRepository.save(pet);
+	}
+
+	/**
+	 * 날짜 변환 메소드
+	 * String 타입의 date를 Date 타입으로 변경
+	 * @param date
+	 * @return Date
+	 */
+	private Date parseDate(String date) {
+		try {
+			return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+		} catch (ParseException e) {
+			throw new DateNotFoundException();
+		}
 	}
 }
