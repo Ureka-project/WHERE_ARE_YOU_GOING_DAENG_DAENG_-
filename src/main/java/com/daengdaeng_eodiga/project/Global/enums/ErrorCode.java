7a7cdbd5fc226a4ab80fd8e0128839ae24e558fb
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-11-26 18:10:08 +0900

    feat: 알림 목록 조회 및 확인 커스텀 예외 구현

--- src/main/java/com/daengdaeng_eodiga/project/Global/enums/ErrorCode.java
+++ src/main/java/com/daengdaeng_eodiga/project/Global/enums/ErrorCode.java
@@ -21,7 +21,8 @@ public enum ErrorCode {
 	DUPLICATE_PET(HttpStatus.CONFLICT,"이미 등록된 반려동물입니다."),
 	USER_FAILED_SAVE(HttpStatus.INTERNAL_SERVER_ERROR, "유저 저장에 실패했습니다."),
 	USER_FAILED_DELETE(HttpStatus.INTERNAL_SERVER_ERROR, "유저 삭제에 실패했습니다."),
-	USER_FAILED_ADJUST(HttpStatus.INTERNAL_SERVER_ERROR, "유저 수정에 실패했습니다.");
+	USER_FAILED_ADJUST(HttpStatus.INTERNAL_SERVER_ERROR, "유저 수정에 실패했습니다."),
+	NOTI_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 알립입니다.");
 	private final HttpStatus errorCode;
 	private final String message;
 
