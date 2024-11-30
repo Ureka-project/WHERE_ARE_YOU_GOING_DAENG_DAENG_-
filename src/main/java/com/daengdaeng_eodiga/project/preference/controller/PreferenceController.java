30eca1280163cbe364b2bb38ec2e1804ee7669cc
13wjdgk <13wjdgk@naver.com>
2024-11-30 18:04:58 +0900

    refactor : 선호도 등록시, 유저 정보 조회

--- src/main/java/com/daengdaeng_eodiga/project/preference/controller/PreferenceController.java
+++ src/main/java/com/daengdaeng_eodiga/project/preference/controller/PreferenceController.java
@@ -1,12 +1,13 @@
 package com.daengdaeng_eodiga.project.preference.controller;
 
+import com.daengdaeng_eodiga.project.Global.Security.config.CustomOAuth2User;
 import com.daengdaeng_eodiga.project.Global.dto.ApiResponse;
 import com.daengdaeng_eodiga.project.preference.dto.PreferenceRequestDto;
 import com.daengdaeng_eodiga.project.preference.dto.PreferenceResponseDto;
 import com.daengdaeng_eodiga.project.preference.service.PreferenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +18,25 @@ import java.util.List;
 public class PreferenceController {
     private final PreferenceService preferenceService;
 
-    // TODO : header 로부터 유저정보 가져오는 걸로 변경해야 함
-    private final int hardcodedUserId = 3;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<?>> registerPreference(@RequestBody PreferenceRequestDto preferenceRequestDto) {
-        preferenceService.registerPreference(hardcodedUserId, preferenceRequestDto);
+    public ResponseEntity<ApiResponse<?>> registerPreference(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody PreferenceRequestDto preferenceRequestDto) {
+        int userId = customOAuth2User.getUserDTO().getUserid();
+        preferenceService.registerPreference(userId, preferenceRequestDto);
         return ResponseEntity.ok(ApiResponse.success("preferences insert succesfully"));
     }
 
     @PutMapping
-    public ResponseEntity<ApiResponse<?>> updatePreference(@RequestBody PreferenceRequestDto preferenceRequestDto) {
-        preferenceService.updatePreference(hardcodedUserId, preferenceRequestDto);
+    public ResponseEntity<ApiResponse<?>> updatePreference(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody PreferenceRequestDto preferenceRequestDto) {
+        int userId = customOAuth2User.getUserDTO().getUserid();
+        preferenceService.updatePreference(userId, preferenceRequestDto);
         return ResponseEntity.ok(ApiResponse.success("preferences update succesfully"));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<?>> fetchPreferences(){
-        List<PreferenceResponseDto> response =  preferenceService.fetchPreferences(hardcodedUserId);
+    public ResponseEntity<ApiResponse<?>> fetchPreferences(@AuthenticationPrincipal CustomOAuth2User customOAuth2User){
+        int userId = customOAuth2User.getUserDTO().getUserid();
+        List<PreferenceResponseDto> response =  preferenceService.fetchPreferences(userId);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
