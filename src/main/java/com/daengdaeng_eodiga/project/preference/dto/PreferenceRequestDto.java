1cb5748c7e185a3ac907bcde0aec03bdf71141c2
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-12-03 01:48:26 +0900

    refactor: 주석 제거

--- src/main/java/com/daengdaeng_eodiga/project/preference/dto/PreferenceRequestDto.java
+++ src/main/java/com/daengdaeng_eodiga/project/preference/dto/PreferenceRequestDto.java
@@ -11,7 +11,7 @@ public class PreferenceRequestDto {
     @NotBlank(message = "그룹코드가 필요함")
     private String preferenceInfo;
 
-    @NotEmpty(message = "공통코드 Set이 존재해야 함") // 빈 Set 방지
+    @NotEmpty(message = "공통코드 Set이 존재해야 함")
     @Size(min = 1, message = "공통코드는 최소 1개의 값을 가져야 함")
     private Set<String> preferenceTypes;
 }
