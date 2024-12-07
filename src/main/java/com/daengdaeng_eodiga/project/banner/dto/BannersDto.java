b15df41b5f0a27a53903fe32b812ed475e72d2ba
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-12-07 13:02:04 +0900

    refactor: 변경된 엔티티에 따른 응답 dto 수정

--- src/main/java/com/daengdaeng_eodiga/project/banner/dto/BannersDto.java
+++ src/main/java/com/daengdaeng_eodiga/project/banner/dto/BannersDto.java
@@ -4,20 +4,24 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class BannerDetailDto {
+public class BannersDto {
+    private int eventId;
     private String eventImage;
     private String eventName;
-    private String placeName;
     private String eventDescription;
+    private String placeName;
+    private String placeAddress;
     private String startDate;
     private String endDate;
 
     @Builder
-    public BannerDetailDto(String eventImage, String eventName, String placeName, String eventDescription, String startDate, String endDate) {
+    public BannersDto(int eventId, String eventImage, String eventName, String eventDescription, String placeName, String placeAddress, String startDate, String endDate) {
+        this.eventId = eventId;
         this.eventImage = eventImage;
         this.eventName = eventName;
-        this.placeName = placeName;
         this.eventDescription = eventDescription;
+        this.placeName = placeName;
+        this.placeAddress = placeAddress;
         this.startDate = startDate;
         this.endDate = endDate;
     }
