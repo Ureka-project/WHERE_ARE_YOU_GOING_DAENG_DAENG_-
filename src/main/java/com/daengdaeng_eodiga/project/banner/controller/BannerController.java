b29236d99ce611a45dcadaa1b11b3a6ebc2ea85a
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-12-07 13:03:00 +0900

    refactor: 이벤트 컨트롤러 수정 이벤트 상세조회 api 삭제

--- src/main/java/com/daengdaeng_eodiga/project/banner/controller/BannerController.java
+++ src/main/java/com/daengdaeng_eodiga/project/banner/controller/BannerController.java
@@ -1,13 +1,11 @@
 package com.daengdaeng_eodiga.project.banner.controller;
 
 import com.daengdaeng_eodiga.project.Global.dto.ApiResponse;
-import com.daengdaeng_eodiga.project.banner.dto.BannerDetailDto;
-import com.daengdaeng_eodiga.project.banner.dto.BannerListDto;
+import com.daengdaeng_eodiga.project.banner.dto.BannersDto;
 import com.daengdaeng_eodiga.project.banner.service.BannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +18,8 @@ public class BannerController {
     private final BannerService bannerService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<BannerListDto>>> fetchBannerList() {
-        List<BannerListDto> response = bannerService.fetchBannerList();
-        return ResponseEntity.ok(ApiResponse.success(response));
-    }
-
-    @GetMapping("/{eventId}")
-    public ResponseEntity<ApiResponse<BannerDetailDto>> fetchBannerDetail(@PathVariable int eventId) {
-        BannerDetailDto response = bannerService.fetchBannerDetail(eventId);
+    public ResponseEntity<ApiResponse<List<BannersDto>>> fetchBanners() {
+        List<BannersDto> response = bannerService.fetchBanners();
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
