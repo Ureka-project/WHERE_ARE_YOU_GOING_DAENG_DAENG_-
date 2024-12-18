5885dc86ddbe7b690d52af86d5e7838cb03e34ba
13wjdgk <13wjdgk@naver.com>
2024-12-18 11:15:21 +0900

    feat : 중복 방문 예정 일정 예외처리

--- src/main/java/com/daengdaeng_eodiga/project/Global/enums/ErrorCode.java
+++ src/main/java/com/daengdaeng_eodiga/project/Global/enums/ErrorCode.java
@@ -37,7 +37,8 @@ public enum ErrorCode {
 	USER_STORY_NOT_FOUND(HttpStatus.NOT_FOUND, "스토리가 존재하지 않습니다."),
 	USER_LAND_NOT_FOUND(HttpStatus.NOT_FOUND, "유저의 땅이 존재하지 않습니다."),
 	OWNER_HISTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "땅 주인 히스토리에 존재하지 않습니다."),
-	DUPLICATE_REVIEW_DAY(HttpStatus.CONFLICT,"오늘 등록된 리뷰가 있습니다.");
+	DUPLICATE_REVIEW_DAY(HttpStatus.CONFLICT,"오늘 등록된 리뷰가 있습니다."),
+	DUPLICATE_VISIT(HttpStatus.CONFLICT,"동일한 시간대에 등록된 방문 예정 일정이 있습니다.");
 	private final HttpStatus errorCode;
 	private final String message;
 
