b164ee8f67fb456b5bef50f6e852555d0c10b00a
lixxce5017 <lixxce5017@gmail.com>
2024-12-18 09:14:38 +0800

    refator:add log

--- src/main/java/com/daengdaeng_eodiga/project/Global/Security/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/daengdaeng_eodiga/project/Global/Security/config/CustomAuthenticationFailureHandler.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -30,7 +31,7 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         String errorMessage = exception.getMessage();
         String email = null;
-
+        log.error(errorMessage);
         OauthProvider provider = null;
         if (errorMessage != null) {
             if (errorMessage.startsWith("REDIRECT_TO_SIGNUP:")) {
